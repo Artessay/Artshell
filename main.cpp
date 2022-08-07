11f95584df5ce23071a75f5c718d2be6dffb322d
Artessay <rihong.edu@outlook.com>
2022-08-07 23:55:03 +0800

    main.c

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+// 程序：命令行编辑器
+// 作者：邱日宏 3200105842
+
+/**
+ * @file main.cpp
+ * @author 邱日宏 (3200105842@zju.edu.cn)
+ * @brief 主函数
+ * @version 0.1
+ * @date 2022-08-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include "myshell.h"
 
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char *argv[], char **env)
 {
